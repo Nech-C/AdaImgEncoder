56cdc7339e41dd1c62e6375c94e025fb416678e2
Chen <xiaokunchen@outlook.com>
2024-08-13 17:30:11 -0400

    fix: correct _info output

--- AdaImgEncoder/utils/cosine_similarity.py
+++ AdaImgEncoder/utils/cosine_similarity.py
@@ -1,12 +1,37 @@
 import numpy as np
 import evaluate
+import datasets
+_DESCRIPTION = """
+Cosine similarity is a measure of similarity between two non-zero vectors of an inner product space that measures the cosine of the angle between them.
+The cosine of 0° is 1, and it is less than 1 for any other angle.
+It is thus a judgment of orientation and not magnitude: two vectors with the same orientation have a cosine similarity of 1, two vectors at 90° have a similarity of 0, and two vectors diametrically opposed have a similarity of -1, independent of their magnitude.
+"""
 
+
+_KWARGS_DESCRIPTION = """
+Args:
+    predictions: list of list of float or float, list of predictions to score.
+    references: list of list of float or float, list of references to score against.
+Returns:
+    cosine_similarity: float, cosine similarity between predictions and references.
+    
+"""
 class CosineSimilarity(evaluate.Metric):
     def _info(self):
         return evaluate.MetricInfo(
-            description="Cosine similarity between two vectors.",
-            citation="No formal citation.",
-            features=["predictions", "references"],
+            description=_DESCRIPTION,
+            citation="",
+            inputs_description=_KWARGS_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "predictions": 
+                        datasets.Sequence(datasets.Value("float32")),
+                    "references":
+                        datasets.Sequence(datasets.Value("float32")),
+                }
+            ),
+            reference_urls="",
+            
         )
 
     def _compute(self, predictions, references):
